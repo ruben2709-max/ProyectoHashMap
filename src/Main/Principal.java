0899ddbce6acba24cc8012af64c31a742c8cfec6
ruben2709-máximo <ruben.argval2709mail.com>
2026-04-10 16:31:37 -0600

    Se quito el codigo anterior

--- src/Main/Principal.java
+++ src/Main/Principal.java
@@ -13,8 +13,7 @@ public class Principal {
 		
 		Scanner lectura = null;
 		
-		System.out.println("Hola mundo");
-		System.out.println("Tienes un "+ 100);
+		
 		
 		String nombre;
 		float sueldo;
